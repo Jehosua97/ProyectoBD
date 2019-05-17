111fed78276ca036e097b6d95cfb82faed58fe1a
UrielTapia <tapia.uriel@outlook.com>
2019-05-16 23:40:00 -0500

    -- Agregada última vista.

--- Codigo/Vistas.sql
+++ Codigo/Vistas.sql
@@ -12,7 +12,17 @@ FROM material m
 JOIN libro l   ON l.material_id = m.material_id
 JOIN escribe e  ON e.material_id = m.material_id
 JOIN autor a    ON a.autor_id = e.autor_id;
+
 --------------------2.- CHAVIRA     Vista de los catálogos de TESIS.
+CREATE OR REPLACE VIEW vwCatalogoTesis
+AS
+SELECT t.material_id, t.tesis_id, m.titulo, a.autor_id, (a.nombreAutor||' '||a.apPaternoAutor) NombreAutor, t.anoPublicacion, t.director_id
+FROM tesis t
+JOIN escribe e ON t.material_id=e.material_id
+JOIN autor a ON e.autor_id=a.autor_id
+JOIN material m ON t.material_id=m.material_id
+JOIN ejemplar On t.material_id=ejemplar.material_id
+JOIN directorTesis d ON t.director_id=d.director_id;
 
 --------------------3.- JOYA        Vista de los catálogos de AUTOR con su tipo de material.
 CREATE OR REPLACE VIEW vwCatalogoAutor(
@@ -31,6 +41,7 @@ select titulo,noEjemplar,descripcione
 from material
 join ejemplar using(material_id)
 join estatus using(estatus_id);
+
 --------------------5.- OSCAR       Vista de LECTOR por tipo.
 
 CREATE OR REPLACE VIEW vwCatalogoLector(
