44fe96fbaaafb9627325dc2056b5391acf0f3758
UrielTapia <tapia.uriel@outlook.com>
2019-05-19 21:13:26 -0500

    .

--- Codigo/Poblacion de tablas.sql
+++ Codigo/Poblacion de tablas.sql
@@ -96,12 +96,12 @@ EXEC AltaEjemplar('M1');
 EXEC AltaEjemplar('M1');
 EXEC AltaEjemplar('M2');
 EXEC AltaEjemplar('M2');
-EXEC AltaEjemplar('M2');
-EXEC AltaEjemplar('M3');
-EXEC AltaEjemplar('M4');
-EXEC AltaEjemplar('M5');
-EXEC AltaEjemplar('M6');
-EXEC AltaEjemplar('M7');
+EXEC AltaEjemplar('M12');
+EXEC AltaEjemplar('M13');
+EXEC AltaEjemplar('M14');
+EXEC AltaEjemplar('M15');
+EXEC AltaEjemplar('M16');
+
 
 
 --tipo lector
@@ -145,10 +145,10 @@ EXEC altaprestamo('L1', 'EJ1', 'M2');
 EXEC altaprestamo('L3', 'EJ0', 'M4');
 EXEC altaprestamo('L4', 'EJ2', 'M1');
 EXEC altaprestamo('L5', 'EJ2', 'M2');
-EXEC altaprestamo('L6', 'EJ0', 'M3');
-EXEC altaprestamo('L7', 'EJ0', 'M5');
-EXEC altaprestamo('L8', 'EJ0', 'M6');
-EXEC altaprestamo('L9', 'EJ0', 'M7');
+EXEC altaprestamo('L6', 'EJ0', 'M13');
+EXEC altaprestamo('L7', 'EJ0', 'M14');
+EXEC altaprestamo('L8', 'EJ0', 'M16');
+EXEC altaprestamo('L9', 'EJ0', 'M15');
 
 UPDATE prestamo SET fecharesello = SYSDATE - 20, fechaprestamo = SYSDATE - 20, fechavencimiento = SYSDATE - 12 WHERE LECTOR_ID = 'L5'; 
 UPDATE prestamo SET fecharesello = SYSDATE - 40, fechaprestamo = SYSDATE - 40, fechavencimiento = SYSDATE - 20 WHERE LECTOR_ID = 'L6'; 
@@ -158,11 +158,16 @@ UPDATE prestamo SET fecharesello = SYSDATE - 20, fechaprestamo = SYSDATE - 20, f
 --SE TIENE QUE EJECUTAR TRIGGER tgDevolEliminPrest ANTES DE LO SIGUIENTE
 
 --Multas
-DELETE prestamo WHERE LECTOR_ID = 'L5';
-DELETE prestamo WHERE LECTOR_ID = 'L6';
-DELETE prestamo WHERE LECTOR_ID = 'L7';
-DELETE prestamo WHERE LECTOR_ID = 'L8';
-DELETE prestamo WHERE LECTOR_ID = 'L9';
+DELETE prestamo WHERE prestamo_id = 'p0';
+DELETE prestamo WHERE prestamo_id = 'p1';
+DELETE prestamo WHERE prestamo_id = 'p2';
+DELETE prestamo WHERE prestamo_id = 'p3';
+DELETE prestamo WHERE prestamo_id = 'p4';
+DELETE prestamo WHERE prestamo_id = 'p5';
+DELETE prestamo WHERE prestamo_id = 'p6';
+DELETE prestamo WHERE prestamo_id = 'p7';
+DELETE prestamo WHERE prestamo_id = 'p8';
+DELETE prestamo WHERE prestamo_id = 'p9';
 
 ALTER TRIGGER tgRevisarResello ENABLE;
 
